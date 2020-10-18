c7568818d92a3f1cec982d6b58c17f49f3d6eab1
ice19 <2235907780@qq.com>
2020-10-19 01:16:44 +0800

    1019---3-绘制默认头像-css书写

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="login-l90-wrap">
-    <div class="login-box"></div>
+    <div class="login-box">
+      <!-- 显示图片区域 -->
+      <div class="touxiang-box">
+        <img src="../assets/logo.png" alt="登录" />
+      </div>
+      <!-- 显示表单区域 -->
+    </div>
   </div>
 </template>
 
@@ -29,5 +35,25 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  .touxiang-box {
+    width: 130px;
+    height: 130px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    overflow: hidden;
+    border-radius: 50%;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    box-shadow: 0 0 8px #ccc;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
 }
 </style>
