0881dd2a2f3f978406f0d5c249f9b90ac0a2379e
ice19 <2235907780@qq.com>
2020-10-21 01:35:46 +0800

    axios请求封装，添加拦截器

--- src/main.js
+++ src/main.js
@@ -1,25 +1,19 @@
 import Vue from 'vue';
-import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // import './plugins/element';
 import App from './App.vue';
 import router from './router';
+import axios from './utils/myAxios';
 
 import './assets/css/global.css';
 import './assets/fonts/iconfont.css';
 
-axios.defaults.baseURL = 'http://m.leijiuling.com/api/private/v1/';
 Vue.prototype.$http = axios;
 
 Vue.use(ElementUI);
 
 new Vue({
   router,
-  data() {
-    return {
-      abc: '我是根组件，通过$root访问',
-    };
-  },
   render: (h) => h(App),
 }).$mount('#app');
---
+++ src/utils/myAxios.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://m.leijiuling.com/api/private/v1/';
+// 路由白名单
+const whiteList = ['/login', '/registry', '/code'];
+// 添加请求拦截器
+axios.interceptors.request.use(
+  function(config) {
+    // 在发送请求之前做些什么
+    console.log(config);
+    // if (config.url !== '/login') {
+    if (!whiteList.includes(config.url)) {
+      config.headers.Authorization = window.sessionStorage.getItem('token');
+    }
+    return config;
+  },
+  function(error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
+
+// 添加响应拦截器
+axios.interceptors.response.use(
+  function(response) {
+    // 对响应数据做点什么
+    return response;
+  },
+  function(error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
