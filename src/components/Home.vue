fbdda6f27a3d45ad62a5ed322c455cc99949f1a4
ice19 <2235907780@qq.com>
2020-10-21 01:11:01 +0800

    美化主页header区域

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="">
-    <el-header>header<el-button @click="loginOut">退出</el-button></el-header>
+    <el-header>
+      <div class="con-logo">
+        <img src="../assets/heima.png" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="loginOut">退出</el-button>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -25,9 +31,27 @@ export default {
   height: 100%;
 }
 .el-header {
-  background-color: #2b4b6b;
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .con-logo {
+    margin-left: -20px;
+    display: flex;
+    align-items: center;
+    img {
+      height: 60px;
+    }
+    span {
+      margin-left: 20px;
+      // vertical-align: middle;
+      // text-align: middle;
+      font-size: 22px;
+      color: #fff;
+    }
+  }
 }
 .el-aside {
-  background-color: #2b4b6b;
+  background-color: #333744;
 }
 </style>
