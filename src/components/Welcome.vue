b98652d7cf3c475c30fb74d5e3131e632e3036e9
ice19 <2235907780@qq.com>
2020-10-21 02:28:22 +0800

    home组件 添加路由占位符，书写子路由

---
+++ src/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    欢迎组件11
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Login from '../components/Login.vue';
 import Home from '../components/Home.vue';
+import Welcome from '../components/Welcome.vue';
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,13 @@ const routes = [
   {
     path: '/home',
     component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome,
+      },
+    ],
   },
 ];
 
