11d265d316721ac23bb091ba1b636af8fd71e080
ice19 <2235907780@qq.com>
2020-10-21 23:59:34 +0800

    修改用户状态

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -28,7 +28,7 @@
         <el-table-column prop="role_name" label="角色"> </el-table-column>
         <el-table-column prop="mg_state" label="状态">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state"> </el-switch>
+            <el-switch v-model="scope.row.mg_state" @change="toogleStatus(scope.row)"> </el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作">
@@ -96,6 +96,13 @@ export default {
     handleCurrentChange(val) {
       this.pagenum = val;
       this.getUserList();
+    },
+    async toogleStatus(val) {
+      console.log(val);
+      const { data: res } = await this.$http.put(`/users/${val.id}/state/${val.mg_state}`);
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+
+      return this.$message.success(res.meta.msg);
     },
   },
 };
