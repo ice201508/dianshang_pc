12c25c14e9050fc5332edd51aa719a98743e20d0
ice19 <2235907780@qq.com>
2020-10-26 02:05:25 +0800

    分别定义动态参数和静态数据的data值，以及渲染table表格

--- src/components/Params.vue
+++ src/components/Params.vue
@@ -27,11 +27,69 @@
           <el-row>
             <el-button type="primary" size="mini" :disabled="parentCateBtn">添加参数</el-button>
           </el-row>
+          <el-table :data="manyParamsData" border stripe style="width: 100%">
+            <el-table-column type="expand" width="50">
+              <template>
+                <div>
+                  123
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column label="#" type="index" width="50"> </el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"> </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  type="primary"
+                  icon="el-icon-edit"
+                  @click="manyHandleEdit(scope.row)"
+                  >编辑</el-button
+                >
+                <el-button
+                  size="mini"
+                  type="danger"
+                  icon="el-icon-delete"
+                  @click="manyHandleDelete(scope.row)"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
         <el-tab-pane label="静态属性" name="only">
           <el-row>
             <el-button type="primary" size="mini" :disabled="parentCateBtn">添加属性</el-button>
           </el-row>
+          <el-table :data="onlyParamsData" border stripe style="width: 100%">
+            <el-table-column type="expand" width="50">
+              <template>
+                <div>
+                  123
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column label="#" type="index" width="50"> </el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"> </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  type="primary"
+                  icon="el-icon-edit"
+                  @click="onlyHandleEdit(scope.row)"
+                  >编辑</el-button
+                >
+                <el-button
+                  size="mini"
+                  type="danger"
+                  icon="el-icon-delete"
+                  @click="onlyHandleDelete(scope.row)"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
@@ -46,6 +104,8 @@ export default {
       parentCate: [],
       parentCateOptions: [],
       activeName: 'many',
+      manyParamsData: [],
+      onlyParamsData: [],
     };
   },
   created() {
@@ -58,25 +118,35 @@ export default {
       this.parentCateOptions = res.data;
     },
     async getParamsList() {
-      const { data: res } = await this.$http.get(
-        `categories/${this.parentCate[this.parentCate.length - 1]}/attributes`,
-        {
-          params: { sel: this.activeName },
-        }
-      );
-      console.log(res);
-    },
-    parentCateHandleChange(val) {
-      if (val.length !== 3) {
+      if (this.parentCate.length !== 3) {
         this.parentCate = [];
         return null;
       }
+      let id = this.parentCate[this.parentCate.length - 1];
+      const { data: res } = await this.$http.get(`categories/${id}/attributes`, {
+        params: { sel: this.activeName },
+      });
 
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+
+      if (this.activeName === 'many') {
+        this.manyParamsData = res.data;
+      } else {
+        this.onlyParamsData = res.data;
+      }
+    },
+    // 这个是级联选择框的数据变化
+    parentCateHandleChange(val) {
       this.getParamsList();
     },
+    // 这个函数是点击切换tab按钮发生的变化
     handleClick() {
       this.getParamsList();
     },
+    manyHandleEdit() {},
+    manyHandleDelete() {},
+    onlyHandleEdit() {},
+    onlyHandleDelete() {},
   },
   computed: {
     parentCateBtn() {
